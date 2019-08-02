f2619f24e27ba97c94824a0efa2274d6d4849bba
Markus Viola <markus.xamuel.viola@gmail.com>
2019-08-02 18:39:20 +0900

    modified columns and added relationship between user and message tables

--- app/Message.php
+++ app/Message.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['message'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
