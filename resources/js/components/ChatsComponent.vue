517e8d7196c94e200edbfd2901c0546f8765f181
Markus Viola <markus.xamuel.viola@gmail.com>
2019-08-03 20:22:13 +0900

    created message box layout file

--- resources/js/components/ChatsComponent.vue
+++ resources/js/components/ChatsComponent.vue
@@ -1,15 +1,26 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
+    <div class="row">
+        <div class="col-8">
+            <div class="card card-default">
+                <div class="card-header">Messages</div>
+                <div class="card-body p-0">
+                    <ul class="list-unstyled" style="height: 300px; overflow-y: scroll">
+                        <li class="p-2">
+                            <strong>Harish</strong>
+                            message text
+                        </li>
+                    </ul>
                 </div>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="Enter your message"
+                    class="form-control">
+                <span class="text-">user is typing...</span>
             </div>
+        </div>
+        <div class="col-4">
+
         </div>
     </div>
 </template>
