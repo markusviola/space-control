abcf5e746096b6637fd03cd648a8dc626eed0ae1
Markus Viola <markus.xamuel.viola@gmail.com>
2019-08-09 17:13:25 +0900

    removed dropdown datepicker library from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,9 +20,6 @@ Vue.use(VCalendar, {
   componentPrefix: 'v'
 });
 
-// Use v-dropdown datepicker component
-Vue.use(DropdownDatepicker);
-
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
