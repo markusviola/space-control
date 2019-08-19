2d500ec321fdaa5f4dea78b916c60a10ae96cb94
oseiryth <markus.xamuel.viola@gmail.com>
2019-08-19 18:02:02 +0900

    changed & modified type table migration

--- database/migrations/2019_08_19_085909_create_types_table.php
+++ database/migrations/2019_08_19_085909_create_types_table.php
@@ -15,6 +15,7 @@ class CreateTypesTable extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
