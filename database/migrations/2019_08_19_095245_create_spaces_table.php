d41d2a1b4884aa2d91565cb32c8db21880291f05
oseiryth <markus.xamuel.viola@gmail.com>
2019-08-19 18:53:29 +0900

    added migration & model for Space model

--- database/migrations/2019_08_19_095245_create_spaces_table.php
+++ database/migrations/2019_08_19_095245_create_spaces_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBulkSpacesTable extends Migration
+class CreateSpacesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBulkSpacesTable extends Migration
      */
     public function up()
     {
-        Schema::create('bulk_spaces', function (Blueprint $table) {
+        Schema::create('spaces', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateBulkSpacesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bulk_spaces');
+        Schema::dropIfExists('spaces');
     }
 }
