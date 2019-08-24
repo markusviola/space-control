d0fd2033f3ec472774d1cd132376668bce0a5441
oseiryth <markus.xamuel.viola@gmail.com>
2019-08-24 15:33:07 +0900

    space form data can now be stored in form table

--- app/Http/Controllers/FormsController.php
+++ app/Http/Controllers/FormsController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Form;
+use App\BulkSpace;
 use Illuminate\Http\Request;
+use App\Form;
+use App\Schedule;
+use App\Space;
+use App\User;
+use DateTime;
 
 class FormsController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -35,8 +41,48 @@ class FormsController extends Controller
      */
     public function store(Request $request)
     {
-        // dd(request()->name);
-        dd(json_decode(request()->dates, true));
+        $startDates = array();
+        $endDates = array();
+        $dates = json_decode(request()->dates, true);
+
+        foreach($dates as $date) {
+            $startEpoch = strtotime($date['startDateTime']);
+            $endEpoch = strtotime($date['endDateTime']);
+            array_push($startDates, $this->phpDateConverter($startEpoch));
+            array_push($endDates, $this->phpDateConverter($endEpoch));
+        }
+
+        $form = new Form();
+
+        if (request()->user_id) {
+            $user = User::where('id', request()->user_id)->first();
+            $form->user_id = $user->id;
+            $form->name = $user->name;
+            $form->furigana = $user->furigana;
+            $form->gender = $user->gender;
+            $form->birthday = $user->birthday;
+            $form->address = $user->address;
+            $form->email = $user->email;
+        } else {
+            $form->name = request()->name;
+            $form->furigana = request()->furigana;
+            $form->gender = request()->gender;
+            $form->birthday = request()->birthday;
+            $form->address = request()->address;
+            $form->email = request()->email;
+        }
+
+        $form->will_stay = false;
+        if (request()->will_stay) {
+            $form->will_stay = true;
+        }
+
+        $form->reason = request()->reason;
+        $form->user_count = request()->user_count ?? 1;
+        $form->type_id = request()->type_id;
+        $form->save();
+
+        return redirect()->route('home');
     }
 
     /**
@@ -82,5 +128,12 @@ class FormsController extends Controller
     public function destroy(Form $form)
     {
         //
+    }
+    // 2019-08-15T07:00:00.000Z
+    public function phpDateConverter($time) {
+        return date(
+            'Y-m-d H:i:s',
+            $time
+        );
     }
 }
