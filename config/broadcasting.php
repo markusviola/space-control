4410b7026ffa24458e9e5b86578496ae9103db5a
oseiryth <markus.xamuel.viola@gmail.com>
2019-08-25 23:02:56 +0900

    set the broadcasting to live for websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,30 +31,28 @@ return [
     'connections' => [
 
         // Actual Pusher API
-        // 'pusher' => [
-        //     'driver' => 'pusher',
-        //     'key' => env('PUSHER_APP_KEY'),
-        //     'secret' => env('PUSHER_APP_SECRET'),
-        //     'app_id' => env('PUSHER_APP_ID'),
-        //     'options' => [
-        //         'cluster' => env('PUSHER_APP_CLUSTER'),
-        //         'useTLS' => true,
-        //     ],
-        // ],
-
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                // 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'useTLS' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
             ],
         ],
+
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'host' => '127.0.0.1',
+        //         'port' => 6001,
+        //         'scheme' => 'http'
+        //     ],
+        // ],
 
         'redis' => [
             'driver' => 'redis',
