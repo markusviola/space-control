b149f7a99dfa67df1207326907ca749db700bbb1
oseiryth <markus.xamuel.viola@gmail.com>
2019-09-02 15:34:38 +0900

    modified model for resertvation status

--- database/migrations/2019_09_02_063142_create_statuses_table.php
+++ database/migrations/2019_09_02_063142_create_statuses_table.php
@@ -15,6 +15,7 @@ class CreateStatusesTable extends Migration
     {
         Schema::create('statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
