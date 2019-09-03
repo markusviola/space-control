0e11da276d6c4a499863e4c9a527c33dce234529
oseiryth <markus.xamuel.viola@gmail.com>
2019-09-03 21:33:01 +0900

    modified index render data for reservation item details

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -2,23 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Discovery;
 use App\Form;
-use App\Reservation;
+use App\Space;
+use App\Status;
 use App\Type;
 use Illuminate\Http\Request;
 
 class ReservationsController extends Controller
 {
     public function index() {
+
         $isReserveList = true;
         $typeId = (int) request()->query('type');
         $types = Type::all();
-        $approvedForms = Form::where('type_id', $typeId)
+        $spaces = Space::all();
+        $discoveries = Discovery::all();
+        $statuses = Status::all();
+
+        $approvedForms = Form::with([
+                'reservation',
+                'reservation.status',
+                'reservation.discovery',
+            ])
+            ->where('type_id', $typeId)
             ->latest()
             ->get();
 
         return view('home',
-            compact('isReserveList', 'types', 'typeId', 'approvedForms'));
+            compact(
+                'isReserveList',
+                'types',
+                'spaces',
+                'discoveries',
+                'statuses',
+                'typeId',
+                'approvedForms'
+            )
+        );
     }
 
     public function update($id) {
