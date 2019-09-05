8c40180082835dbadfade01a764094feec8943a2
oseiryth <markus.xamuel.viola@gmail.com>
2019-09-05 23:03:02 +0900

    initially created method for getting payment records

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Reservation;
 
 class PaymentsController extends Controller
 {
-    //
+    public function getPaymentRecords($typeId) {
+
+        $reservations = Reservation::whereHas('form', function($q) use($typeId) {
+            $q->where('type_id', $typeId);
+        })->whereYear('actual_paydate', '>' , date('2000'))
+            ->where('payment_cost', '!=', 0)
+            ->get();
+
+        return response()->json($reservations);
+    }
 }
