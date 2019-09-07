72536e509bc9134f6f428e64aa3cc505064ff94a
oseiryth <markus.xamuel.viola@gmail.com>
2019-09-07 18:01:39 +0900

    tweaked payment records interface styling

--- resources/views/admin/payments.blade.php
+++ resources/views/admin/payments.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 <div class="container">
-    Hello World
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8">
+            <div class="card shadow neutral-round">
+                <div class="card-body">
+                    <div class="text-center">
+                        <h5 class="alt-neutral">Payment Records</h5>
+                        <div class="text-muted">Check how your businses has been going.</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
