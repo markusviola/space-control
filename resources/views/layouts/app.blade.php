188b3f85682ac19b730db3eb947a5bc544421fdf
Markus Viola <markus.xamuel.viola@gmail.com>
2019-11-14 15:02:49 +0900

    renamed the app to スペコン from スペースコントロール

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>スペースコントロール</title>
+    <title>スペコン</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <strong class="text-primary"><i class="fas fa-rocket fa-lg mr-2 text-secondary"></i>スペースコントロール</strong>
+                    <strong class="text-primary"><i class="fas fa-rocket fa-lg mr-2 text-secondary"></i>スペコン</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
