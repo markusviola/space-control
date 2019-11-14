3f4e105315594a60b92a58420cc98fcc2e1455a7
Markus Viola <markus.xamuel.viola@gmail.com>
2019-11-14 09:49:22 +0900

    initialized template for space information component

---
+++ resources/js/components/SpaceInfo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Space Info
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
