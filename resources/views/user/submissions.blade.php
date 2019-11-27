2fe55b2389783510fb99d93b96be584b8885e0c5
Markus Viola <markus.xamuel.viola@gmail.com>
2019-11-27 11:28:29 +0900

    reworked submission list template, to be rendered as an app section

--- resources/views/user/submissions.blade.php
+++ resources/views/user/submissions.blade.php
@@ -1,31 +1,40 @@
-<div class="side-wrapper mb-2">
-    <div class="card shadow-sm side-card neutral-round">
-        <div class="text-center mt-3">
-            <h5 class="alt-neutral">提出リスト</h5>
-            <div class="text-muted">管理者に問い合わせよう！</div>
-            <hr>
-        </div>
-        <div class="container mb-0 side-list">
-            @if (sizeof($forms) > 0)
-                @foreach ($forms as $form)
-                    @if ($form->user_id == auth()->user()->id)
-                        <a class="anti-neutral" href="{{ route('messenger', $form->id)}}">
-                            <h6>
-                            <strong>{{ $form->type->name }}</strong>
-                            </h6>
-                        </a>
-                        <div class="text-muted">
-                            提出日 {{ $form->created_at }}
-                        </div>
-                        <hr>
-                    @endif
-                @endforeach
-            @else
-                <div class="text-muted text-center mt-4 mb-4">
-                    何も予約フォームは提出されていません。
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card shadow neutral-round">
+                <div class="card-body">
+                    <div class="text-center">
+                        <h4 class="alt-neutral">提出フォームリスト</h4>
+                        <span class="text-dark">管理者に問い合わせよう！</span>
+                    </div>
+                    <div class="formlist-column">
+                        @if (sizeof($forms) > 0)
+                            @foreach ($forms as $form)
+                                <div class="container py-3 panel-highlight" onclick="toMessenger({!! json_encode($form->id) !!})" >
+                                    <div class="row">
+                                        <h6>
+                                            <strong>{{ $form->post->title }}</strong>
+                                        </h6>
+                                        <div class="text-muted">
+                                            提出日 {{ $form->created_at }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr class="m-0 p-0">
+                            @endforeach
+                        @else
+                            <div class="text-muted text-center mt-4 mb-4">
+                                何も予約要求は提出されていません。
+                            </div>
+                        @endif
+                    </div>
+                    <hr class="mt-0">
                 </div>
-            @endif
+            </div>
         </div>
-        <hr class="mt-0">
     </div>
 </div>
+@endsection
